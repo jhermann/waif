0633d7351fb6d4d08e04f407ebb1e8a1790af265
jhermann <jh@web.de>
2013-09-06 00:53:49 +0200

    complete sentence (i should learn to save before a commit)

--- bash/basics.sh
+++ bash/basics.sh
@@ -2,7 +2,7 @@
 # Basic helpers for any shell script
 #
 # You don't want a dependency for these, and they're pretty simple
-# and stable, so just break the rules and cut'n'copy them
+# and stable, so just break the rules and cut'n'copy them.
 #
 
 fail() { # fail with error message on stderr and exit code 1
