834f00e954643962a9233d07f2bf6c2dc80d6cfa
jhermann <jh@web.de>
2013-09-06 00:52:20 +0200

    Basic helpers for any shell script

---
+++ bash/basics.sh
@@ -0,0 +1,12 @@
+#
+# Basic helpers for any shell script
+#
+# You don't want a dependency for these, and they're pretty simple
+# and stable, so just break the rules and cut'n'copy them
+#
+
+fail() { # fail with error message on stderr and exit code 1
+    echo >&2 "ERROR:" "$@"
+    exit 1
+}
+
